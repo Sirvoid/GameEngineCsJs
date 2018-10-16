fa9465b74bb13f677780230c5ce58bb2e5e3173d
alexandre <alexandred015@outlook.com>
2018-10-15 21:52:18 -0400

    Started working on graphics.

--- Game-Engine-CS-JS/Game.cs
+++ Game-Engine-CS-JS/Game.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using Bridge;
-using Newtonsoft.Json;
+using Bridge.Html5;
+using GameEngineJS.Graphics;
 
 namespace GameEngineJS
 {
     public class Game
     {
-        
+        public HTMLCanvasElement canvas { get; set;}
+        public Drawer drawer { get; set; }
+
+        public Game(int canvasID,string color) {
+            canvas = Document.QuerySelector<HTMLCanvasElement>("canvas#" + canvasID);
+            drawer = new Drawer(canvas);
+        }
     }
 }
--- Game-Engine-CS-JS/Graphics/Drawer.cs
+++ Game-Engine-CS-JS/Graphics/Drawer.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using Bridge;
+using Bridge.Html5;
 using Newtonsoft.Json;
 
 namespace GameEngineJS.Graphics
 {
     public class Drawer
     {
-        void Draw() {
+        public CanvasRenderingContext2D ctx { get; set; }
 
+        public Drawer(HTMLCanvasElement canvas) {
+            ctx = (CanvasRenderingContext2D)canvas.GetContext("2d");
+        }
+
+        public void Draw(int x, int y, int w, int h, dynamic img) => Draw(x, y, w, h, 0, img, false, 1);
+        public void Draw(int x,int y,int w, int h, float r, dynamic img, bool follow, float alpha) {
+            ctx.DrawImage(img, x, y, w, h);
         }
     }
 }
